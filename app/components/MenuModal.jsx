a7d86ea8c126e78fb180bed32c62e7f7a4769013
aswin-codes <aswinraaj.ps@gmail.com>
2024-10-26 17:48:31 +0530

    feat : configured routes

--- app/components/MenuModal.jsx
+++ app/components/MenuModal.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Close, Logo, Phone, PhoneIcon, UserIcon } from "./Icons";
+import Link from "next/link";
 
 const MenuModal = ({ isVisible, onClose }) => {
     return (
         <div
-            className={`fixed top-0 z-50 left-0 h-full w-full bg-white flex justify-between flex-col z-50 transform transition-transform duration-300 ease-in-out py-6 px-10 ${isVisible ? "translate-x-0" : "-translate-x-full"
+            className={`fixed top-0  left-0 h-full w-full bg-white flex justify-between flex-col z-50 transform transition-transform duration-300 ease-in-out py-6 px-10 ${isVisible ? "translate-x-0" : "-translate-x-full"
                 }`}
         >
             <div>
                 <div className="flex items-center justify-between w-full mb-5">
-                    <div className="text-black"><Logo size={1.2} /></div>
+                    <Link href={"/"}><div className="text-black"><Logo size={1.2} /></div></Link>
                     <div onClick={onClose} className="cursor-pointer text-black"><Close /></div>
                 </div>
-                <div className="mt-5 text-base text-black cursor-pointer font-thin ">About Us</div>
+                <Link href={"/about-us"}><div className="mt-5 text-base text-black cursor-pointer font-thin ">About Us</div></Link>
                 <div className="mt-5  text-black cursor-pointer">Mortgage</div>
-                <div className="mt-5  text-black cursor-pointer">Start</div>
+                <Link href={"/start"}><div className="mt-5  text-black cursor-pointer">Start</div></Link>
                 <div className="bg-successGreen rounded-full mt-3 text-black cursor-pointer flex items-center justify-center gap-3 py-2 ">
                     <Phone />
                     <p> Call us anytime at <span className="underline">1234567890</span></p>
@@ -27,7 +28,7 @@ const MenuModal = ({ isVisible, onClose }) => {
                 </div>
                 <div className="flex gap-2 text-black items-center justify-center border border-gray-300 py-5 rounded-full hover:border-primary hover:border-2 transition-all duration-200 ease-in-out mt-4 cursor-pointer">
                     <p>Sign in</p>
-                    <span><UserIcon/></span>
+                    <span><UserIcon /></span>
                 </div>
             </div>
         </div>
